21c9ef5f77856b09acdfbba239e8ddf2e5ac8c5e
Alexander Carlin <ahcarlin@gmail.com>
2018-10-16 21:41:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,9 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   
-  def initialize(*args)
-    self.name = args[0]
-    self.grade = args[1] 
+  def initialize(name, grade, id=nil)
+    self.name = name
+    self.grade = grade
     @id = id
   end 
   
