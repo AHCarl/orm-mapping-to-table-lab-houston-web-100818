e62cc7d485bee816616f042a6f06a533cfdd0690
Alexander Carlin <ahcarlin@gmail.com>
2018-10-16 21:30:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,8 +28,8 @@ class Student
   
   def save
     sql = <<-SQL
-    INSERT INTO students VALUES (?, ?, ?)
+    INSERT INTO students (name, grade) VALUES (?, ?)
     SQL
-    DB[:conn].execute(sql, id, name, grade)
+    DB[:conn].execute(sql, name, grade)
   end
 end
